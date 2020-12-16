9188f1bf7cc6f143a6781dab5575473e58da7cb1
revliscano <revliscano@gmail.com>
2020-12-15 21:22:18 -0400

    Abort button added

--- app/GUI/widgets.py
+++ app/GUI/widgets.py
@@ -19,6 +19,7 @@ class Root(Widget):
 
     def start_pomodoro(self, event):
         self.restart()
+        self.switch_buttons()
         self.timer = Clock.schedule_interval(
             self.run_timer,
             EVERY_SECOND
@@ -28,6 +29,10 @@ class Root(Widget):
         self.seconds_elapsed = 0
         self.ids.timer_label.bring_back_to_zero()
         self.ids.up_to_label.update(self.pomodoro_counter.next_lapse)
+
+    def switch_buttons(self):
+        self.ids.start_next_lapse_button.switch()
+        self.ids.abort_lapse_button.switch()
 
     def run_timer(self, event):
         self.seconds_elapsed += 1
@@ -39,8 +44,16 @@ class Root(Widget):
         return self.seconds_elapsed == self.pomodoro_counter.next_lapse
 
     def finish_pomodoro(self):
-        Clock.unschedule(self.timer)
         self.pomodoro_counter.update_after_lapse_ends()
+        self.tear_down()
+
+    def tear_down(self):
+        Clock.unschedule(self.timer)
+        self.switch_buttons()
+
+    def abort_lapse(self, event):
+        self.tear_down()
+        self.restart()
 
 
 class UpToLabel(Label):
@@ -63,6 +76,16 @@ class TimerLabel(Label):
         self.text = str(SecondsFormatter(seconds))
 
 
-class StartNextLapse(Button):
+class Switchable:
+    def switch(self):
+        self.disabled = not self.disabled
+
+
+class StartNextLapse(Button, Switchable):
     def on_parent(self, obj, parent):
         self.bind(on_press=parent.start_pomodoro)
+
+
+class AbortLapse(Button, Switchable):
+    def on_parent(self, obj, parent):
+        self.bind(on_press=parent.abort_lapse)
--- main.py
+++ main.py
@@ -3,19 +3,13 @@ from kivy.app import App
 from app.settings import setup
 from app.GUI.widgets import Root
 from app.app.pomodoro_counter import (
-    PomodoroCounter, PomodoroCounterConfiguration, DEFAULT_CONFIGURATION
+    PomodoroCounter, DEFAULT_CONFIGURATION
 )
 
 
 class PymodoroApp(App):
     def build(self):
-        configuration = PomodoroCounterConfiguration(
-            pomodoro_duration=2,
-            break_duration=1,
-            long_break_duration=3,
-            long_break_after=2
-        )
-        pomodoro_counter = PomodoroCounter(configuration)
+        pomodoro_counter = PomodoroCounter(DEFAULT_CONFIGURATION)
         return Root(pomodoro_counter=pomodoro_counter)
 
 
