9f7d2ef46714454cddfca95f1fb0768fe1e21643
revliscano <revliscano@gmail.com>
2020-12-23 23:55:20 -0400

    Custom session modal added

--- app/ui/title_screen.py
+++ app/ui/title_screen.py
@@ -1,4 +1,5 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.modalview import ModalView
 from kivy.uix.button import Button
 from kivy.app import App
 
@@ -7,10 +8,23 @@ class TitleScreen(Screen):
     pass
 
 
-class StartSession(Button):
+class NormalSession(Button):
     def on_parent(self, obj, parent):
         self.bind(on_press=self.start_session)
 
     def start_session(self, event):
         app = App.get_running_app()
         app.switch_to_main_screen()
+
+
+class CustomSession(Button):
+    def on_parent(self, obj, parent):
+        self.bind(on_press=self.open_modal)
+
+    def open_modal(self, event):
+        modal = CustomSessionModal()
+        modal.open()
+
+
+class CustomSessionModal(ModalView):
+    pass
